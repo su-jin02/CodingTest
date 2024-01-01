aee13cc8dcdeb5c986dea32caafb202996abef50
sj <hera4224@naver.com>
2024-01-01 22:39:47 +0900

    강의 섹션6.부분집합구하기

---
+++ 1.py
@@ -0,0 +1,18 @@
+#입력 값을 깊이 우선 탐색 전위순회방식 출력
+import sys
+
+def DFS(n):
+    if n == x+1:
+        for i in range(1, x + 1):
+            if (ch[i] == 1):
+                print(i, end=' ')
+        print()
+    else:
+        ch[n] = 1
+        DFS(n+1)
+        ch[n] = 0
+        DFS(n+1)
+
+x = int(sys.stdin.readline())
+ch = [0] * (x+1)
+DFS(1)
