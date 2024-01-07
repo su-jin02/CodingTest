7629a4e4fa37610d601bc3c8f9f8f66003b21af4
su-jin02 <hera4224@naver.com>
2024-01-08 00:08:58 +0900

    강의섹션6.조합구하기

--- 1.7.py
+++ 1.7.py
@@ -1,22 +1,20 @@
 #1부터 x까지의 번호가 적힌 구슬을 y개 뽑는 경우의 수
 import sys
-n,m = map(int, sys.stdin.readline().split())
+x,y = map(int, sys.stdin.readline().split())
 result = 0
 
-def DFS(L, s):
-    global cnt
-    if L == m:
-        for i in range(m):
-            print(res[i], end=' ')
+def DFS(n,m):  #(가지 뻗는 첫번째 수, 레벨)
+    global ch, result
+    if m == y:
+        for i in ch:
+            print(i, end=' ')
         print()
-        cnt += 1
+        result += 1
     else:
-        for i in range(s, n + 1):
-            res[L] = i
-            DFS(L + 1, i + 1)
+        for i in range(n,x+1):
+            ch[m] = i
+            DFS(i+1,m+1)
 
-
-res = [0] * (n + 1)
-cnt = 0
-DFS(0, 1)
-print(cnt)
+ch= [0]*y
+DFS(1,0)
+print(result)
