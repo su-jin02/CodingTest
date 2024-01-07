bc3990762b9b1ec89e8850076312414b52dd1f7f
su-jin02 <hera4224@naver.com>
2024-01-08 00:27:20 +0900

    강의섹션6.동전교환

--- 1.6.py
+++ 1.6.py
@@ -1,22 +1,24 @@
+#가장 적은 수의 동전으로 거스름돈 주기
 import sys
-result = 1000
+result = 10000
 
-def DFS(n,m):
+def DFS(n,sum): #(동전개수, 동전합)
     global result
-    if (m >= b):
-        if(m == b):
-            result = min(result, n)
+    if n>result or sum>b: #이미 나온 최솟값보다 개수가 커지거나 구하려는 값을 넘어가면 탐색 중지
+        return
 
-
+    if(sum == b):
+        if(result > n):
+            result = n
     else:
         for i in lst:
-            if(n+1 < result):
-                DFS(n+1,m+i)
+            DFS(n+1, sum+i)
 
 
 a= int(sys.stdin.readline())
 lst= list(map(int, sys.stdin.readline().split()))
 b= int(sys.stdin.readline())
 lst.sort(reverse=True)
+print(lst)
 DFS(0,0)
 print(result)
