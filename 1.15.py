c03c19626149a3b5480596ec62b96ebd56386f80
su-jin02 <hera4224@naver.com>
2024-01-16 18:11:03 +0900

    강의섹션7. 최대점수 구하기

--- 1.15.py
+++ 1.15.py
@@ -7,15 +7,26 @@ for _ in range(x):
     a,b = map(int, input().split())
     ch.append([a,b,a/b])
 
-ch.sort(key=lambda x:-x[2])
 
+ch.sort(key=lambda x:-x[2])
 def DFS(n, score, time):
     if n == x:
         return score
-
-    if(time+ch[n][1] > y):
+    if time+ch[n][1] > y:
         return DFS(n+1, score, time)
     else:
         return DFS(n+1, score+ch[n][0], time+ch[n][1])
+
+
+# #해설 (시간대비 점수 계산 없이 탐색)
+# def DFS(n, score, time):
+#     if time > y:
+#         return
+#     if n == x:
+#         if result > score:
+#             result = score
+#     else:
+#         DFS(n+1, score+ch[n][0], time+ch[n][1])
+#         DFS(n+1, score, time)
 
 print(DFS(0,0,0))
