a7dd814f40bb9e51531895ba8bb4e780ed76fe3d
su-jin02 <hera4224@naver.com>
2024-01-24 21:44:49 +0900

    강의섹션7. 섬나라 아일랜드

---
+++ 1.24.py
@@ -0,0 +1,36 @@
+# 강의섹션7. 섬나라 아일랜드
+import sys
+from collections import deque
+input = sys.stdin.readline
+a = int(input())
+ch= [list(map(int, input().split())) for _ in range(a)]
+res = 0
+q = deque()
+dx=[-1, -1, 0, 1, 1, 1, 0, -1]
+dy=[0, 1, 1, 1, 0, -1, -1, -1]
+
+def canMove(x, y):
+    if x < 0 or x > a-1:
+        return False
+    if y < 0 or y > a-1:
+        return False
+    return True
+
+def BFS():
+    while q:
+        x,y = q.popleft()
+        for i in range(8):
+            tx = x+dx[i]
+            ty = y+dy[i]
+            if canMove(tx,ty) and ch[tx][ty] == 1:
+                ch[tx][ty] = 0
+                q.append([tx,ty])
+
+for i in range(a):
+    for j in range(a):
+        if(ch[i][j] == 1):
+            q.append([i,j])
+            ch[i][j] = 0
+            BFS()
+            res += 1
+print(res)
