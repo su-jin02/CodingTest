2ed9fefa5f90c7173fb9a1260bc7730fd4ff292e
su-jin02 <hera4224@naver.com>
2024-01-28 20:13:31 +0900

    강의섹션7. 단지 번호 붙이기(BFS)

--- 1.26.py
+++ 1.26.py
@@ -1,4 +1,4 @@
-# 강의섹션7. 단지 번호 붙이기
+# 강의섹션7. 단지 번호 붙이기(BFS)
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -19,22 +19,22 @@ def canMove(x, y):
 
 for i in range(a):
     for j in range(a):
-        if(ch[i][j] == 1):
+        if ch[i][j] == 1:
             q.append([i,j])
             ch[i][j] = 0
-            res_list.append(1)
+            res = 1
             while q:
                 x, y = q.popleft()
-                for i in range(4):
-                    tx = x + dx[i]
-                    ty = y + dy[i]
+                for k in range(4):
+                    tx = x + dx[k]
+                    ty = y + dy[k]
                     if canMove(tx, ty) and ch[tx][ty] == 1:
                         ch[tx][ty] = 0
-                        res_list[res] += 1
                         q.append([tx, ty])
-            res += 1
+                        res+=1
+            res_list.append(res)
 
-print(res)
+print(len(res_list))
 res_list.sort()
 for i in res_list:
     print(i)
