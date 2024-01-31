bd53d974aa833502a3b67d7fb922d753e6bc0fb6
su-jin02 <hera4224@naver.com>
2024-01-31 23:15:48 +0900

    강의섹션8. 최대 부분 증가수열

---
+++ 1.31.py
@@ -0,0 +1,17 @@
+# 강의섹션8. 최대 부분 증가수열
+import sys
+input = sys.stdin.readline
+a = int(input())
+ch = list(map(int, input().split()))
+dy = [0] * a
+dy[0] = 1
+res = 0
+for i in range(1, a):
+    for j in range(0, i):
+        if ch[i] > ch[j] and dy[i] < dy[j]:
+            dy[i] = dy[j]
+    dy[i] += 1
+    if res < dy[i]:
+        res = dy[i]
+
+print(res)
