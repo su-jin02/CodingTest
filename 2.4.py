7707a873529dcf61e2ffb2d97612f8f918ac39f0
su-jin02 <hera4224@naver.com>
2024-02-04 21:43:42 +0900

    강의섹션8. 가방문제

---
+++ 2.4.py
@@ -0,0 +1,12 @@
+# 강의섹션8. 가방문제
+import sys
+input = sys.stdin.readline
+n=int(input())
+coin=list(map(int, input().split()))
+m=int(input())
+dy=[1000]*(m+1);
+dy[0]=0
+for i in range(n):
+    for j in range(coin[i], m+1):
+        dy[j]=min(dy[j], dy[j-coin[i]]+1)
+print(dy[m])
