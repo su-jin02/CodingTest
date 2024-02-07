f335793827ee784e3bce4389923fc217617c3a27
su-jin02 <hera4224@naver.com>
2024-02-08 00:36:03 +0900

    강의섹션8. 최대점수 구하기

--- 2월/2.3.py
+++ 2월/2.3.py
@@ -1,10 +1,11 @@
-# 강의섹션8. 가방문제
+# 강의섹션8. 최대점수 구하기
 import sys
 input = sys.stdin.readline
-n, m=map(int, input().split())
-dy=[0]*(m+1);
+n, m = map(int, input().split())
+dy=[0]*(m+1) #해당 인덱스 시간동안 풀 수 있는 최대점수
 for i in range(n):
-    w, v=map(int, input().split())
-    for j in range(w, m+1):
-        dy[j]=max(dy[j], dy[j-w]+v)
+    score, time = map(int, input().split())
+    for j in range(time, m+1):
+        dy[j] = max(dy[j], dy[j-time]+score)
+
 print(dy[m])
