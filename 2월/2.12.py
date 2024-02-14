f59ccb6fe92bec51427765e3aea4190e4d0f53d9
su-jin02 <hera4224@naver.com>
2024-02-14 13:24:08 +0900

    백준 10026 - BFS 적록색약

--- 2월/2.12.py
+++ 2월/2.12.py
@@ -1,49 +1,67 @@
-#백준 10026 - BFS
+#백준 10026 - BFS 적록색약
 import sys
+from collections import deque
 sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
+a = int(input())
+board = [list(map(str, input().strip())) for _ in range(a)]
+visited1 = [[0] * a for _ in range(a)]
+visited2 = [[0] * a for _ in range(a)]
+res = 0
+q1 = deque()
+q2 = deque()
+dx=[-1, 0, 0, 1]
+dy=[0, 1, -1, 0]
 
-n = int(input().rstrip())
-matrix = [list(input().rstrip()) for _ in range(n)]
-visited = [[False] * n for _ in range(n)]
+def canMove(x, y):
+    if x < 0 or x > a-1:
+        return False
+    if y < 0 or y > a-1:
+        return False
+    return True
 
-three_cnt, two_cnt = 0, 0
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+def rgb(target): #적록색약 X
+    while q1:
+        x, y = q1.popleft()
+        for k in range(4):
+            tx = x + dx[k]
+            ty = y + dy[k]
+            if canMove(tx, ty) and board[tx][ty] == target and visited1[tx][ty] == 0:
+                visited1[tx][ty] = 1
+                q1.append([tx, ty])
 
-def dfs(x,y):
-    #현재 색상 좌표를 방문해준다.
-    visited[x][y] = True
-    current_color = matrix[x][y]
+def r_gb(): #적록색약 O
+    while q2:
+        x, y = q2.popleft()
+        for k in range(4):
+            tx = x + dx[k]
+            ty = y + dy[k]
+            if canMove(tx, ty) and visited2[tx][ty] == 0:
+                if board[tx][ty] == 'R' or board[tx][ty] == 'G':
+                    visited2[tx][ty] = 1
+                    q2.append([tx, ty])
 
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-        if (0 <= nx < n) and (0 <= ny < n):
-            #현재 좌표의 색상과 상하좌우 좌표에 있는 색상이 같으면 dfs로 넣어준다.
-            if visited[nx][ny]==False:
-                if matrix[nx][ny] == current_color:
-                    dfs(nx,ny)
+cnt1 = 0
+cnt2 = 0
+for i in range(a):
+    for j in range(a):
+        if visited1[i][j] == 0: #빠르게 계산하기 위해 위로 뺌
+            if board[i][j] == 'R' or board[i][j] == 'G':
+                q1.append([i,j])
+                visited1[i][j] = 1
+                cnt1 += 1
+                rgb(board[i][j])
+                if visited2[i][j] == 0:
+                    q2.append([i,j])
+                    visited2[i][j] = 1
+                    cnt2 += 1
+                    r_gb()
 
-for i in range(n):
-    for j in range(n):
-        # 방문하지 않은 좌표이면 dfs로 넣어준다.
-        if visited[i][j]==False:
-            dfs(i,j)
-            three_cnt += 1
+            else:
+                q1.append([i, j])
+                visited1[i][j] = 1
+                cnt1 += 1
+                cnt2 += 1
+                rgb(board[i][j])
 
-#R을 G로 바꾸어준다. --> 적록색약은 같은 색으로 보기 때문에
-for i in range(n):
-    for j in range(n):
-        if matrix[i][j]=='R':
-            matrix[i][j]='G'
-
-visited = [[False] * n for _ in range(n)]
-
-for i in range(n):
-    for j in range(n):
-        if visited[i][j] == False:
-            dfs(i,j)
-            two_cnt += 1
-
-print(three_cnt,two_cnt)
+print(cnt1,cnt2)
