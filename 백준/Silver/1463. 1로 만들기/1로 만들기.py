01b3fdb1a03540a3c74f028671ee0c3bf1822f65
su-jin02 <108807044+su-jin02@users.noreply.github.com>
2024-02-27 03:34:17 +0900

    [Silver III] Title: 1로 만들기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,23 +1,40 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 x=int(input())
-dy=[1000]*(x+1)
-dy[x]=0
 
-def DFS(n):
-    if n <= 1:
-        return
-    if n % 3 == 0 and dy[n//3] > dy[n] + 1:
-        dy[n//3] = dy[n] + 1
-        DFS(n//3)
-    if n % 2 == 0 and dy[n//2] > dy[n] + 1:
-        dy[n//2] = dy[n] + 1
-        DFS(n // 2)
+#내가 짠 코드
+# dy=[1000]*(x+1)
+# dy[x]=0
+# def DFS(n):
+#     if n <= 1:
+#         return
+#     if n % 3 == 0 and dy[n//3] > dy[n] + 1:
+#         dy[n//3] = dy[n] + 1
+#         DFS(n//3)
+#     if n % 2 == 0 and dy[n//2] > dy[n] + 1:
+#         dy[n//2] = dy[n] + 1
+#         DFS(n // 2)
+#
+#     if dy[n - 1] > dy[n] + 1:
+#         dy[n - 1] = dy[n] + 1
+#         DFS(n - 1)
+# DFS(x)
+# print(dy[1])
 
-    if dy[n - 1] > dy[n] + 1:
-        dy[n - 1] = dy[n] + 1
-        DFS(n - 1)
+# 풀이 참고
+dp = {1: 0}
+def cal(n):
+    if n in dp.keys():
+        return dp[n]
+    if (n % 3 == 0) and (n % 2 == 0):
+        dp[n] = min(cal(n // 3) + 1, cal(n // 2) + 1)
+    elif n % 3 == 0:
+        dp[n] = min(cal(n // 3) + 1, cal(n - 1) + 1)
+    elif n % 2 == 0:
+        dp[n] = min(cal(n // 2) + 1, cal(n - 1) + 1)
+    else:
+        dp[n] = cal(n - 1) + 1
 
-DFS(x)
-print(dy[1])
+    return dp[n]
+
+print(cal(x))
