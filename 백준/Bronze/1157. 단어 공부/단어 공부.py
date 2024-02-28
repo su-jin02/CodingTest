fb5cebe22761efb3cf22db1482271d3f4ee857f2
su-jin02 <108807044+su-jin02@users.noreply.github.com>
2024-02-29 03:18:15 +0900

    [Bronze I] Title: 단어 공부, Time: 88 ms, Memory: 34404 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -2,8 +2,16 @@ import sys
 from collections import Counter
 input = sys.stdin.readline
 x = input().strip().upper()
-counter = Counter(x).most_common()
-if len(counter) > 1 and counter[0][1] == counter[1][1]:
-    print('?')
-else:
-    print(counter[0][0])
+a=[]
+for i in range(65,91):
+    a.append(x.count(chr(i)))
+if a.count(max(a)) > 1:
+    print("?")
+else: print(chr(a.index(max(a))+65))
+
+#수정전
+# counter = Counter(x).most_common()
+# if len(counter) > 1 and counter[0][1] == counter[1][1]:
+#     print('?')
+# else:
+#     print(counter[0][0])
