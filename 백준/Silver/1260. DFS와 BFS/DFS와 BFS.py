ba0044f9e62ae327389dc24e1db4b334b96e8495
su-jin02 <108807044+su-jin02@users.noreply.github.com>
2024-03-01 03:52:15 +0900

    [Silver II] Title: DFS와 BFS, Time: 72 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -4,36 +4,36 @@ from collections import deque
 input = sys.stdin.readline
 n, m, v = map(int, sys.stdin.readline().split())
 q = deque()
-graph = [[0]*(n+1) for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+DFS_ch = [0] * (n + 1)
+BFS_ch = [0] * (n + 1)
 
 def DFS(x):
-    for i in range(1, n + 1):
-        if graph[x][i] == 1 and ch[i] == 0:
-            print(i, end=' ')
-            ch[i] = 1
+    print(x, end=' ')
+    DFS_ch[x] = 1
+    for i in graph[x]:
+        if DFS_ch[i] == 0:
             DFS(i)
 
 def BFS(v):
-    ch = [0] * (n + 1)
     q.append(v)
-    ch[v] = 1
+    BFS_ch[v] = 1
     while q:
         x = q.popleft()
-        for i in range(1,n+1):
-            if graph[x][i] == 1 and ch[i] == 0:
+        for i in graph[x]:
+            if BFS_ch[i] == 0:
                 q.append(i)
-                ch[i] = 1
+                BFS_ch[i] = 1
         print(x, end=' ')
-
 
 for _ in range(m):
     a,b = map(int, sys.stdin.readline().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
+    graph[a].append(b)
+    graph[b].append(a)
 
-ch = [0] * (n + 1)
-ch[v] = 1
-print(v, end=' ')
+for i in range(n + 1):
+    graph[i].sort()
+    
 DFS(v)
 print()
 BFS(v)
