4d05b986915f73c6306b7683736fd3fefb107476
su-jin02 <108807044+su-jin02@users.noreply.github.com>
2024-03-15 02:34:38 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -3,7 +3,10 @@ input = sys.stdin.readline
 board = [list(map(str, input().strip())) for _ in range(5)]
 for i in range(15):
     for j in range(5):
-        try:
-            print(board[j][i], end='')
-        except:
-            continue
+        #처음 코드
+        # try:
+        #     print(board[j][i], end='')
+        # except:
+        #     continue
+        if i < len(board[j]):
+            print(board[j][i], end="")
