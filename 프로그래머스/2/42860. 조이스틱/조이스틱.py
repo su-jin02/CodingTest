d4353ed4fb7a573c8b04224ba58375176eacd148
su-jin02 <108807044+su-jin02@users.noreply.github.com>
2024-06-10 17:14:26 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,19 @@
+#풀이 참조
+def solution(name):
+    len_name = len(name)
+    answer = 0
+    rem = 0
+    cnt = 1e9
+    for i in range(len_name):
+        if (name[i] != 'A'):
+            cal = ord(name[i])
+            answer += min(91-cal, cal-65)
+
+            if i == 0:
+                continue
+            tmp = rem + len_name - i
+            cnt = min(cnt, min(tmp+rem, tmp+len_name-i))
+            # 우측이동 후 좌측으로 이동, 좌측이동 후 우측 이동인지
+            rem = i
+    cnt = min(rem, cnt) #다 훑기 vs 특정 숫자만
+    return answer + cnt
