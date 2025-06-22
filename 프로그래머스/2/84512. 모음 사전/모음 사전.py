87e123f2505bac37a7d797dec2970909030fd19b
su-jin02 <108807044+su-jin02@users.noreply.github.com>
2025-06-22 18:01:27 +0900

    [level 2] Title: 모음 사전, Time: 10.04 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,19 +1,29 @@
+from collections import deque
+
 def solution(word):
-    from itertools import product
-    words = []
-    answer = {}
-    for i in range(5):
-        for x in list(product("AEIOU", repeat=i+1)):
-            words.append(''.join(x))
-    words.sort()
-    for i, x in enumerate(words):
-        answer[x] = i + 1
-    
-    return answer[word]
-
-    
-
-    
-    
-    
-    
+    dic = ['A', 'E', 'I', 'O', 'U']
+    diction = {'A':0, 'E':1, 'I':2, 'O':3, 'U':4}
+    dq = deque([])
+    answer = 0
+    prev = 0
+    while "".join(dq) != word:
+        print(dq)
+        if len(dq) == 5:
+            x = dq.pop()
+            if x == 'U':
+                while True:
+                    y = dq.pop()
+                    if y != 'U':
+                        prev = diction[y]+1
+                        dq.append(dic[prev])
+                        answer += 1
+                        prev = 0
+                        break
+            else:
+                prev += 1
+                dq.append(dic[prev])
+                answer += 1
+        else:
+            dq.append(dic[prev])
+            answer += 1
+    return answer
