aa9a09c153a65ac9b995d82a3c8568abd793c5b1
su-jin02 <108807044+su-jin02@users.noreply.github.com>
2025-06-22 18:59:08 +0900

    [level 2] Title: 모음 사전, Time: 1.78 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,29 +1,55 @@
 from collections import deque
 
 def solution(word):
-    dic = ['A', 'E', 'I', 'O', 'U']
-    diction = {'A':0, 'E':1, 'I':2, 'O':3, 'U':4}
-    dq = deque([])
     answer = 0
-    prev = 0
-    while "".join(dq) != word:
-        print(dq)
-        if len(dq) == 5:
-            x = dq.pop()
-            if x == 'U':
-                while True:
-                    y = dq.pop()
-                    if y != 'U':
-                        prev = diction[y]+1
-                        dq.append(dic[prev])
-                        answer += 1
-                        prev = 0
-                        break
-            else:
-                prev += 1
-                dq.append(dic[prev])
-                answer += 1
-        else:
-            dq.append(dic[prev])
+    ans = 0
+    dic = ['A', 'E', 'I', 'O', 'U']
+
+    def dfs(x):
+        nonlocal answer, ans
+        if x == word:
+            ans = answer
+            return
+        if len(x) == 5:
+            return
+        for i in dic:
             answer += 1
-    return answer
+            dfs(x + i)
+
+    dfs("")
+    return ans
+    
+    # diction = {'A':0, 'E':1, 'I':2, 'O':3, 'U':4}
+    # dq = deque([])
+    # prev = 0
+    # while "".join(dq) != word:
+    #     print(dq)
+    #     if len(dq) == 5:
+    #         x = dq.pop()
+    #         if x == 'U':
+    #             while True:
+    #                 y = dq.pop()
+    #                 if y != 'U':
+    #                     prev = diction[y]+1
+    #                     dq.append(dic[prev])
+    #                     answer += 1
+    #                     prev = 0
+    #                     break
+    #         else:
+    #             prev += 1
+    #             dq.append(dic[prev])
+    #             answer += 1
+    #     else:
+    #         dq.append(dic[prev])
+    #         answer += 1
+    # return answer
+
+#자리 가중치
+# def solution(word):
+#     weight = [781, 156, 31, 6, 1]
+#     code   = {'A': 0, 'E': 1, 'I': 2, 'O': 3, 'U': 4}
+
+#     idx = 0
+#     for i, ch in enumerate(word):
+#         idx += code[ch] * weight[i] + 1    # 자리값 기여 + 자기 자신
+#     return idx
