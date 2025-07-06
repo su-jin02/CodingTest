c7d1eca9efb53a3026a63c985e0a9cc9f050982a
su-jin02 <108807044+su-jin02@users.noreply.github.com>
2025-07-06 17:18:28 +0900

    [level 3] Title: 단어 변환, Time: 0.28 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,36 @@
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+    
+    answer = len(words)
+
+    def dfs(word, find):
+        print(word, find)
+        nonlocal answer, words
+        for i in range(len(words)):
+            if find[i] == 0:
+                diff = 0
+                #단어가 다른개 1개인지 판단
+                for j in range(len(word)):
+                    if diff > 1:
+                        break
+                    if word[j] != words[i][j]:
+                        diff += 1
+                #맞다면 탐색
+                if diff == 1:
+                    if words[i] == target:
+                        print('-----',word, words[i], find)
+                        temp = 0
+                        for i in find:
+                            if i == 1:
+                                temp += 1
+                        answer = min(answer, temp+1)
+                    else:
+                        find[i] = 1
+                        dfs(words[i], find)
+                        find[i] = 0
+
+    dfs(begin, [0] * len(words))
+    return answer
+        
+        
