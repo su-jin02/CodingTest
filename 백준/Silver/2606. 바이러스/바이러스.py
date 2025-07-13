45bb352def673a3340440a26da8fbc7468d1b0da
su-jin02 <108807044+su-jin02@users.noreply.github.com>
2025-07-13 22:55:48 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,28 @@
+import sys
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v = int(sys.stdin.readline())
+e = int(sys.stdin.readline())
+parent = [0] * (v + 1)
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+for i in range(1, v + 1):
+    find_parent(parent, i)
+print(parent.count(parent[1]) - 1)
